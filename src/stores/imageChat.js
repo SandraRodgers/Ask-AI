12478efa062251b9802ea050f01d1396942ac464
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-08-05 17:50:50 -0500

    lesson 9 end

--- src/stores/imageChat.js
+++ src/stores/imageChat.js
@@ -1,9 +1,52 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useImageChatStore = defineStore('imageChat', () => {
-  function createPrompt() {}
+  const prompt = ref([])
+  const miniGPTResponse = ref('')
+  const question = ref('')
+  const imageURL = ref('')
+  const questionAnswerList = ref([])
 
-  function clearChat() {}
+  function createPrompt() {
+    tokenizeStore.checkTokens(imageURL.value + question.value)
+    fetch('http://localhost:3000/replicate-chain', {
+      method: 'POST',
+      body: JSON.stringify({
+        image: imageURL.value,
+        prompt: question.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        miniGPTResponse.value = data.message
+        questionAnswerList.value.push({
+          question: question.value,
+          answer: data.message
+        })
+        question.value = ''
+      })
+  }
 
-  return {}
+  function clearChat() {
+    prompt.value = []
+    miniGPTResponse.value = ''
+    question.value = ''
+    imageURL.value = ''
+    questionAnswerList.value = []
+  }
+
+  return {
+    prompt,
+    miniGPTResponse,
+    createPrompt,
+    imageURL,
+    question,
+    clearChat,
+    questionAnswerList
+  }
 })
