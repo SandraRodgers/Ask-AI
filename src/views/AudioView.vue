52f2d9c5b709dc5ff30ef80a7199855adc05921a
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-08-15 14:35:22 -0500

    lesson 5 start

--- src/views/AudioView.vue
+++ src/views/AudioView.vue
@@ -1,3 +1,16 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <article class="grid grid-cols-1 lg:grid-cols-2 gap-x-8">
+    <div class="mt-4">
+      <h3>Choose audio that has some speech that you would like to ask questions about.</h3>
+      <section class="my-4">
+        <!-- Uploader -->
+      </section>
+      <!-- Transcriber -->
+    </div>
+    <div class="min-h-80 flex flex-col justify-between">
+      <!-- Chat Window -->
+    </div>
+  </article>
+</template>
